c49f35102f4e8a74f2c8757486c2a409d5ab7d24
Komron Khisomov <normok_9595@bk.ru>
2024-07-21 12:28:10 +0500

    arranging files

---
+++ test/daoTest.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {daoScript} from "../src/daoScript.s.sol";
+
+contract daoTest is Test {
+    daoScript public script;
+
+    function setUp() public {
+        counter = new Counter();
+        counter.setNumber(0);
+
+}
