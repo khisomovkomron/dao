083cb5ee5d97aaf0469e5e9ae024360c09dc7815
Komron Khisomov <normok_9595@bk.ru>
2024-07-22 19:27:01 +0500

    Implementing DAOInterface:

---
+++ src/DAOInterface.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.8.20;
+
+import {Token} from "./TokenCreation.sol";
+
+contract DAOInterface {
+    uint256 constant minProposalDatePeriod = 2 weeks;
+    uint256 constant quorumHalvingPeriod = 25 weeks;
+    uint256 constant executeProposalPeriod = 10 days;
+    uint256 constant preSupportTime = 2 days;
+    uint256 constant maxDepositDivisor = 100;
+
+    Proposal[] public proposals;
+    uint256 public minQuorumDivisor;
+    uint256 public lastTimeQuorumMet;
+
+    address public curator;
+    mapping(address => bool) public allowedRecipient;
+    mapping(address => uint256) public blocked;
+    mapping(address => uint256[]) public votingRegister;
+
+    uint256 public proposalDeposit;
+    uint256 sumOfProposalDeposits;
+
+    struct Proposal {
+        address recipient;
+        uint amount;
+        string description;
+        uint votingDeadline;
+        bool open;
+        bool proposalPassed;
+        bytes32 proposalHash;
+        uint proposalDeposit;
+        bool newCurator;
+        bool preSupport;
+        uint yea;
+        uint nay;
+        mapping(address => bool) votedYes;
+        mapping(address => bool) votedNo;
+        address creator;
+    }
+
+    function() payable;
+
+    function newProposal() public returns (uint _proposalId);
+
+    function checkProposalCode() constant returns (bool _codeCheckout);
+
+    function vote(uint _proposalId, bool _supportsProposal);
+
+    function executeProposal(uint _proposalId, bytes _transactionData) returns (bool _success);
+    
+    function newContract(address _newContract);
+
+    function changeAllowedRecipients(address _recipient, bool _allowd) external returns (bool _success);
+
+    function changeProposalDeposit(uint _proposalDeposit) external;
+
+    function halveMinQuorum() returns (bool _success);
+
+    function numberOfProposals() constant returns (uint _numberOfProposals);
+
+    
+
+
+
+}
