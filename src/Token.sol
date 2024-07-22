07a8ddd36238a11b48df6ca06cf2f685387a3db6
Komron Khisomov <normok_9595@bk.ru>
2024-07-22 23:58:10 +0500

    TokenInterface

---
+++ src/Token.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.8.20;
+
+contract Plutocracy {
+    function getOrModifyBlocked(address _account) returns (bool);
+}
+
+contract TokenInterface {
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    string public name;
+    string public symbol;
+    uint  public decimals;
+
+    /// Total amount of tokens
+    uint256 public totalSupply;
+
+    /// Governance contract
+    Plutocracy plutocracy;
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance);
+
+    /// @notice Send `_amount` tokens to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _amount The amount of tokens to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _amount) returns (bool success);
+
+    /// @notice Send `_amount` tokens to `_to` from `_from` on the condition it
+    /// is approved by `_from`
+    /// @param _from The address of the origin of the transfer
+    /// @param _to The address of the recipient
+    /// @param _amount The amount of tokens to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _amount) returns (bool success);
+
+    /// @notice `msg.sender` approves `_spender` to spend `_amount` tokens on
+    /// its behalf
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _amount The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _amount) returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens of _owner that _spender is allowed
+    /// to spend
+    function allowance(
+        address _owner,
+        address _spender
+    ) constant returns (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _amount);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _amount
+    );
+}
+
+}
