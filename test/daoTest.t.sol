dc203ce932d6af8dde1964d602181aadb33b7a72
Komron Khisomov <normok_9595@bk.ru>
2024-07-27 11:45:50 +0500

    testChairman

--- test/daoTest.t.sol
+++ test/daoTest.t.sol
@@ -3,9 +3,22 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {daoScript} from "../script/daoScript.s.sol";
+import {DAO} from "../src/DAO.sol";
 
 contract daoTest is Test {
+    DAO public dao;
+    address payable public vendingMachine;
     daoScript public script;
+    string[] public proposalTypes;
 
-    function setUp() public {}
+    function setUp() public {
+        vendingMachine = payable(address(0x123));
+        proposalTypes = ["buy_cupcakes", "no_cupcakes"];
+
+        dao = new DAO(vendingMachine, 86400, proposalTypes);
+    }
+
+    function testChairman() public view {
+        assertEq(dao.chairPerson(), address(this), "chairPerson not set correctly");
+    }
 }
